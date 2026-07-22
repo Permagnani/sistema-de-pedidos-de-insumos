9584343e2451680bdee83eb95d856fa49483b331
Permagnani <guribeiropermagnani@gmail.com>
2026-07-22 16:45:05 -0300

    debug: lista contas correntes Omie

--- api/debug.js
+++ api/debug.js
@@ -1,14 +1,14 @@
 module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
 
-  const resp = await fetch('https://app.omie.com.br/api/v1/geral/categorias/', {
+  const resp = await fetch('https://app.omie.com.br/api/v1/geral/contacorrente/', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({
-      call: 'ListarCategorias',
+      call: 'ListarContasCorrentes',
       app_key:    process.env.OMIE_APP_KEY,
       app_secret: process.env.OMIE_APP_SECRET,
-      param: [{ pagina: 1, registros_por_pagina: 100 }],
+      param: [{ pagina: 1, registros_por_pagina: 50 }],
     }),
   });
 
