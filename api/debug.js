d9ca5678d70738173bd52d2f5d47d3b9aaf5e0ae
Permagnani <guribeiropermagnani@gmail.com>
2026-07-22 16:40:26 -0300

    debug: lista categorias Omie

---
+++ api/debug.js
@@ -0,0 +1,17 @@
+module.exports = async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  const resp = await fetch('https://app.omie.com.br/api/v1/geral/categorias/', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      call: 'ListarCategorias',
+      app_key:    process.env.OMIE_APP_KEY,
+      app_secret: process.env.OMIE_APP_SECRET,
+      param: [{ pagina: 1, registros_por_pagina: 100 }],
+    }),
+  });
+
+  const data = await resp.json();
+  return res.status(200).json(data);
+};
