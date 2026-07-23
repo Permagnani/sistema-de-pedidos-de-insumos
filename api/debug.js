e6b20c4db131df18230ea83cc6d606da7e0f25a9
Permagnani <guribeiropermagnani@gmail.com>
2026-07-23 10:38:03 -0300

    debug: lista produtos Omie para mapear IDs

--- api/debug.js
+++ api/debug.js
@@ -1,17 +1,15 @@
 module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-
-  const resp = await fetch('https://app.omie.com.br/api/v1/geral/contacorrente/', {
+  const resp = await fetch('https://app.omie.com.br/api/v1/geral/produtos/', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({
-      call: 'ListarContasCorrentes',
-      app_key:    process.env.OMIE_APP_KEY,
+      call: 'ListarProdutos',
+      app_key: process.env.OMIE_APP_KEY,
       app_secret: process.env.OMIE_APP_SECRET,
-      param: [{ pagina: 1, registros_por_pagina: 50 }],
+      param: [{ pagina: 1, registros_por_pagina: 100 }],
     }),
   });
-
   const data = await resp.json();
   return res.status(200).json(data);
 };
